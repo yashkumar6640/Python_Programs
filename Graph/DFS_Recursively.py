3aaa2f84d317d63b5744fb63edf1bd7f5f3131cf
yashkumar6640 <yashkumar6640@gmail.com>
2016-10-11 21:00:33 +0530

    Update DFS_Recursively.py

--- Graph/DFS_Recursively.py
+++ Graph/DFS_Recursively.py
@@ -9,16 +9,6 @@ class Graph:
 			self.dfs(graph, next, visited);
 		return visited;
 
-
-
-
-
-
-
-
-
-
-
 graph = {'A': set(['B', 'C']),
              'B': set(['A', 'D', 'E']),
              'C': set(['A', 'F']),
