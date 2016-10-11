e77c0172522072ae6f0c17b63f22ecf755bbd990
Kumar <Yash.Kumar@unisys.com>
2016-10-11 20:59:29 +0530

    First Commit

---
+++ Graph/DFS_Recursively.py
@@ -0,0 +1,32 @@
+class Graph:
+	def __init__(self, graph):
+		self.graph=graph;
+	def dfs(self, graph, start, visited):
+		if start not in visited:
+			print(start);
+			visited.add(start);
+		for next in graph[start]-visited:
+			self.dfs(graph, next, visited);
+		return visited;
+
+
+
+
+
+
+
+
+
+
+
+graph = {'A': set(['B', 'C']),
+             'B': set(['A', 'D', 'E']),
+             'C': set(['A', 'F']),
+             'D': set(['B']),
+             'E': set(['B', 'F']),
+             'F': set(['C', 'E'])
+            }
+
+visited=set();
+g=Graph(graph);
+g.dfs(graph, 'A', visited);
