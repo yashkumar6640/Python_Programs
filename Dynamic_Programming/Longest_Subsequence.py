abe84482df3a94140c86ebd9ee5b0d8cb38e121b
Kumar <Yash.Kumar@unisys.com>
2016-10-16 12:15:52 +0530

    Second Commit

--- Dynamic_Programming/Longest_Subsequence.py
+++ Dynamic_Programming/Longest_Subsequence.py
@@ -7,14 +7,16 @@ def longest_subsequence_length(string1, string2):
 			matrix[i].append(0);
 	for i in range(len(string1) + 1):
 		for j in range(len(string2) + 1):
-			if i==0 and j==0:
+			if i==0 or j==0:
 				matrix[i][j]=0;
+			
+			elif string1[i-1]==string2[j-1]:
+				matrix[i][j]=matrix[i-1][j-1]+ 1;
 			else:
-				if string1[i-1]==string2[j-1]:
-					matrix[i][j]=max(matrix[i-1][j], matrix[i][j-1]) + 1;
-				else:
-					matrix[i][j]=max(matrix[i-1][j], matrix[i][j-1]);
+				matrix[i][j]=max(matrix[i-1][j], matrix[i][j-1]);
 
+	for i in range(len(string1) + 1):
+		print(matrix[i]);
 	return matrix[len(string1)][len(string2)];
 
 
